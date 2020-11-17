801e7cb154159508b73ec89a8e135e4dd72bd85f
RomanHrytsko <romangritsko@gmail.com>
2020-11-18 00:39:12 +0200

    last task in progres

--- Lesson7/index.js
+++ Lesson7/index.js
@@ -288,4 +288,164 @@
 
 // }
 
-document.body.prepend(wrapper);
+// document.body.prepend(wrapper);
+
+//
+//
+//
+//
+// -- взять массив пользователей
+// Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
+// 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
+// 2й - оставляет старше 29 лет включительно
+// 3й - оставляет тех у кого город киев
+// Данные выводить в документ
+let usersWithAddress = [
+  {
+    id: 1,
+    name: "vasya",
+    age: 31,
+    status: false,
+    address: { city: "Lviv", street: "Shevchenko", number: 16 },
+  },
+  {
+    id: 2,
+    name: "petya",
+    age: 30,
+    status: true,
+    address: { city: "Kyiv", street: "Shevchenko", number: 1 },
+  },
+  {
+    id: 3,
+    name: "kolya",
+    age: 29,
+    status: true,
+    address: { city: "Lviv", street: "Shevchenko", number: 121 },
+  },
+  {
+    id: 4,
+    name: "olya",
+    age: 28,
+    status: false,
+    address: { city: "Ternopil", street: "Shevchenko", number: 90 },
+  },
+  {
+    id: 5,
+    name: "max",
+    age: 30,
+    status: true,
+    address: { city: "Lviv", street: "Shevchenko", number: 115 },
+  },
+  {
+    id: 6,
+    name: "anya",
+    age: 31,
+    status: false,
+    address: { city: "Kyiv", street: "Shevchenko", number: 2 },
+  },
+  {
+    id: 7,
+    name: "oleg",
+    age: 28,
+    status: false,
+    address: { city: "Ternopil", street: "Shevchenko", number: 22 },
+  },
+  {
+    id: 8,
+    name: "andrey",
+    age: 29,
+    status: true,
+    address: { city: "Lviv", street: "Shevchenko", number: 43 },
+  },
+  {
+    id: 9,
+    name: "masha",
+    age: 30,
+    status: true,
+    address: { city: "Kyiv", street: "Shevchenko", number: 12 },
+  },
+  {
+    id: 10,
+    name: "olya",
+    age: 31,
+    status: false,
+    address: { city: "Lviv", street: "Shevchenko", number: 16 },
+  },
+  {
+    id: 11,
+    name: "max",
+    age: 31,
+    status: true,
+    address: { city: "Ternopil", street: "Shevchenko", number: 121 },
+  },
+];
+
+// CHECK BOXES
+
+class CheckBoxConstructor {
+  constructor(nameOfId, textInner, arrUsers) {
+    this.nameOfId = nameOfId;
+    this.textInner = textInner;
+    const checkBox = document.createElement("input");
+    const label = document.createElement("label");
+    let divStatus = document.createElement("div");
+
+    if (checkBox.id == "statusCheckBox") {
+      status(arrUsers);
+      console.log(status(arrUsers));
+    }
+
+    divStatus.style.display = "flex";
+
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.setAttribute("id", nameOfId);
+
+    label.setAttribute("for", nameOfId);
+    label.innerHTML = textInner;
+
+    divStatus.appendChild(checkBox);
+    divStatus.appendChild(label);
+    document.body.appendChild(divStatus);
+  }
+  status(arr) {
+    arr.filter((value) => {
+      if (value.status == true) {
+        return true;
+      }
+      console.log(value);
+    });
+  }
+}
+
+let statusCheckBox = new CheckBoxConstructor(
+  "statusCheckBox",
+  "Users with status False"
+);
+let ageCheckBox = new CheckBoxConstructor(
+  "ageCheckBox",
+  "Users older than 29 years age"
+);
+let cityCheckBox = new CheckBoxConstructor(
+  "cityCheckBox",
+  "Users who live in Kyiv"
+);
+
+// let filterStatus = usersWithAddress.filter((value) => {
+//   if (value.status === false) {
+//     return true;
+//   }
+// });
+// console.log(filterStatus);
+
+// let filterAge = usersWithAddress.filter((value) => {
+//   if (value.age > 29) {
+//     return true;
+//   }
+// });
+// console.log(filterAge);
+
+// let filterCity = usersWithAddress.filter((value) => {
+//   if (value.address.city == "Kyiv") {
+//     return true;
+//   }
+// });
