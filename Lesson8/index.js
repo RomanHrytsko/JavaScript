30f529a856e9b8c812ca6d503b8e0895f2593e65
RomanHrytsko <romangritsko@gmail.com>
2020-11-24 18:43:42 +0200

    lesson 8 completed

--- Lesson8/index.js
+++ Lesson8/index.js
@@ -54,39 +54,149 @@
 // Требование : хранить историю своих изменений (даже после перезагрузки страницы).
 // Сверху над текстареа должны появится стрелочки, с помощью которых можно перемещаться по истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
 
-let div = document.createElement("div");
-let buttonSave = document.createElement("button");
-let textarea = document.createElement("textarea");
-let buttonUp = document.createElement("button");
-let buttomDown = document.createElement("button");
-buttonSave.innerHTML = "SAVE";
-buttomDown.innerHTML = "DOWN";
-buttonUp.innerHTML = "UP";
-div.style.display = "flex";
-div.style.flexDirection = "column";
+// let div = document.createElement("div");
+// let buttonSave = document.createElement("button");
+// let textarea = document.createElement("textarea");
+// let buttonUp = document.createElement("button");
+// let buttonDown = document.createElement("button");
+// buttonSave.innerHTML = "SAVE";
+// buttonDown.innerHTML = "DOWN";
+// buttonUp.innerHTML = "UP";
+// div.style.display = "flex";
+// div.style.flexDirection = "column";
 
-buttonSave.onclick = () => {
-  localStorage.setItem(localStorage.length + 1, textarea.value);
-};
+// buttonSave.onclick = () => {
+//   localStorage.setItem(localStorage.length + 1, textarea.value);
+// };
+// textarea.value = localStorage.getItem(localStorage.length);
 
-buttonUp.onclick = () => {
-  let index;
-  for (let key in localStorage) {
-    if (localStorage.hasOwnProperty(key)) {
-      if (localStorage.getItem(key) === textarea.value) {
-        index = key;
-      }
-    }
-  }
-  if (index === localStorage.length.toString()) {
-    buttonUp.style.visibility = "hidden";
-    return;
-  }
-  textarea.value = localStorage.getItem(+index + 1);
-};
+// buttonUp.onclick = () => {
+//   buttonDown.style.visibility = "visible";
+//   let index;
+//   for (let key in localStorage) {
+//     // console.log(key);
+//     if (localStorage.hasOwnProperty(key)) {
+//       if (localStorage.getItem(key) === textarea.value) {
+//         index = key;
+//         console.log(index);
+//       }
+//     }
+//   }
+//   if (index === "1") {
+//     buttonUp.style.visibility = "hidden";
+//     return;
+//   }
+//   textarea.value = localStorage.getItem(index - 1);
+// };
+// buttonDown.onclick = () => {
+//   buttonUp.style.visibility = "visible";
+//   let index;
+//   for (let key in localStorage) {
+//     // console.log(key);
+//     if (localStorage.hasOwnProperty(key)) {
+//       if (localStorage.getItem(key) === textarea.value) {
+//         index = key;
+//         console.log(index);
+//       }
+//     }
+//   }
+//   if (index === localStorage.length.toString()) {
+//     buttonDown.style.visibility = "hidden";
+//     return;
+//   }
+//   textarea.value = localStorage.getItem(+index + 1);
+// };
 
-div.appendChild(buttonUp);
-div.appendChild(textarea);
-div.appendChild(buttomDown);
-div.appendChild(buttonSave);
-document.body.appendChild(div);
+// div.appendChild(buttonUp);
+// div.appendChild(textarea);
+// div.appendChild(buttonDown);
+// div.appendChild(buttonSave);
+// document.body.appendChild(div);
+
+// - Реализуйте записную книгу, хранящую данные в локальном хранилище.
+// Данные которые надо сохранять : ФИО, номер, почта, фирма, отдел, день рождения
+// Данные вводить через соответсвующую форму.
+// --Каждому контакту добавить кнопку для удаления контакта.
+// --Каждому контакту добавить кнопку редактироваиня. При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования, которые уже заполнены данными объекта
+
+// let content = document.getElementById("content");
+// let inputDiv = document.getElementById("inputs");
+// let inputs = document.getElementsByClassName("textinput");
+// let nameInput = document.getElementById("nameInput");
+// let surnameInput = document.getElementById("surnameInput");
+// let phoneInput = document.getElementById("phoneInput");
+// let emailInput = document.getElementById("emailInput");
+
+// let buttonSave = document.getElementById("saveUser");
+// let buttonDelete = document.getElementById("deleteUser");
+// let buttonEdit = document.getElementById("editInfo");
+
+// let blockName = document.createElement("div");
+// let blockSurname = document.createElement("div");
+// let blockPhone = document.createElement("div");
+// let blockEmail = document.createElement("div");
+
+// buttonSave.onclick = () => {
+//   for (let input of inputs) {
+//     if (input.id === "nameInput") {
+//       localStorage.setItem("name", input.value);
+//     } else if (input.id === "surnameInput") {
+//       localStorage.setItem("surname", input.value);
+//     } else if (input.id === "phoneInput") {
+//       localStorage.setItem("phoneNumber", input.value);
+//     } else if (input.id === "emailInput") {
+//       localStorage.setItem("Email", input.value);
+//     }
+//   }
+//   for (let key in localStorage) {
+//     if (key === "name") {
+//       blockName.innerHTML = `User name is ${localStorage.getItem(key)}`;
+//     } else if (key === "surname") {
+//       blockSurname.innerHTML = `User surname is ${localStorage.getItem(key)}`;
+//     } else if (key === "phoneNumber") {
+//       blockPhone.innerHTML = `User phone number is is ${localStorage.getItem(
+//         key
+//       )}`;
+//     } else if (key === "Email") {
+//       blockEmail.innerHTML = `User email is ${localStorage.getItem(key)}`;
+//     }
+//   }
+// };
+// buttonDelete.onclick = () => {
+//   localStorage.clear();
+//   window.location.reload();
+// };
+
+// buttonEdit.onclick = () => {
+//   if (nameInput.value == "") {
+//     localStorage.getItem("name");
+//   } else if (nameInput.value !== localStorage.getItem("name")) {
+//     localStorage.setItem("name", nameInput.value);
+//   }
+//   if (surnameInput.value == "") {
+//     localStorage.getItem("surname");
+//   } else if (surnameInput.value !== localStorage.getItem("surname")) {
+//     localStorage.setItem("surname", surnameInput.value);
+//   }
+//   if (phoneInput.value == "") {
+//     localStorage.getItem("phoneNumber");
+//   } else if (phoneInput.value !== localStorage.getItem("phoneNumber")) {
+//     localStorage.setItem("phoneNumber", phoneInput.value);
+//   }
+//   if (emailInput.value == "") {
+//     localStorage.getItem("Email");
+//   } else if (emailInput.value !== localStorage.getItem("Email")) {
+//     localStorage.setItem("Email", emailInput.value);
+//   }
+
+//   window.location.reload();
+// };
+
+// blockName.innerHTML = localStorage.getItem("name");
+// blockSurname.innerHTML = localStorage.getItem("surname");
+// blockPhone.innerHTML = localStorage.getItem("phoneNumber");
+// blockEmail.innerHTML = localStorage.getItem("Email");
+// content.appendChild(blockName);
+// content.appendChild(blockSurname);
+// content.appendChild(blockPhone);
+// content.appendChild(blockEmail);
